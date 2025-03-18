757a70f6f24223bd9549ad2d2553311a95ca68b9
Jacob Stenson <jacob.stenson@dpdgroup.co.uk>
2025-03-18 22:55:36 +0000

    Add prettier

--- src/pages/api/menu/index.tsx
+++ src/pages/api/menu/index.tsx
@@ -1,10 +1,19 @@
 // @ts-ignore
 export default async function handler(req, res) {
-    switch (req.method) {
-        case "GET":
-            res.status(200).json({ name: "jacob" });
-            break;
-        default:
-            res.status(400).json({ error: "Wrong request method" });
-    }
+  switch (req.method) {
+    case "GET":
+      res.status(200).json({ name: "jacob" });
+      break;
+    case "POST":
+      try {
+        console.log("req.body");
+        console.log(req.body);
+        return res.status(200);
+      } catch (e) {
+        console.error(e);
+        return res.status(500);
+      }
+    default:
+      res.status(400).json({ error: "Wrong request method" });
+  }
 }
